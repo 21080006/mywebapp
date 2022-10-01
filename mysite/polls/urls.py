dc5c1d4e5e0ea42b9dae38772e9a693f2ac7f032
21080006 <21080006@upemor.edu.mx>
2022-10-01 11:10:41 -0500

    agregando la version 2

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name='index'),
-    #path('hola2/',views.hola_dos, name='hola2/'),
-    #path('<int:question_id>/', views.detail, name='detail'),
+    #path('hola2/', views.hola_dos, name='hola2'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
