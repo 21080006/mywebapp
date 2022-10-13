8a97288f2947161bdd151e4b33c7e30c657e7167
21080006 <21080006@upemor.edu.mx>
2022-10-13 18:58:48 -0500

    Proyecto polls, trabajo en clase

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import imp
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'), name='polls')
-    
+    path('polls/', include('polls.urls'), name='polls'),
+    # path('', include('polls.urls'))
 ]
